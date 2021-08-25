62abc1a59450fc7f7fd7001a75c1146a52b0792e
baffoRti <saha14373@gmail.com>
2021-08-25 13:40:33 +0300

    processing dates

--- request_handler/views.py
+++ request_handler/views.py
@@ -1,9 +1,10 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 from xml.etree import ElementTree
 import xmltodict
 import json
 import requests
+from datetime import datetime
 
 file = open("static/XML_data.asp")
 new_json = xmltodict.parse(file.read())
@@ -21,12 +22,24 @@ def Method_A(request):
 
 
 def Method_B(request, code, date_1, date_2):
-    new_date_1 = date_1[-2:] + '/' + date_1[-5:-3] + '/' + date_1[0:4]
-    new_date_2 = date_2[-2:] + '/' + date_2[-5:-3] + '/' + date_2[0:4]
+    try:
+        date_1_obj = datetime.strptime(date_1, "%Y-%m-%d")
+    except ValueError:
+        return HttpResponse("Request error")
+    try:
+        date_2_obj = datetime.strptime(date_2, "%Y-%m-%d")
+    except ValueError:
+        return HttpResponse("Request error")
+    if date_1_obj > date_2_obj:
+        return HttpResponse("Request error")
+    new_date_1 = date_1_obj.strftime("%d/%m/%Y")
+    new_date_2 = date_2_obj.strftime("%d/%m/%Y")
     parent_code = parent_code_by_code(code)
     response = requests.post(
         f"http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1={new_date_1}&date_req2={new_date_2}&VAL_NM_RQ={parent_code}")
     xml_text = ElementTree.fromstring(response.content)
+    if len(xml_text) == 0:
+        return HttpResponse("Request error")
     json_data = {
         "date_1": f"{xml_text[0][1].text.replace(',', '.')[:5]}",
         "date_2": f"{xml_text[-1][1].text.replace(',', '.')[:5]}",
